c05d4d54ed782a73f3676c02e4c931168a7dcaed
Juno <junoplupus@gmail.com>
2026-06-30 18:08:51 -0300

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# https://tutorial.djangogirls.org/pt/django_views/
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
